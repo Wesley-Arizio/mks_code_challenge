6a912f3769c25da63a57cac79ca1741c6575ed26
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-11-27 19:06:23 -0300

    added validation to create movies dto

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateMovieDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 
   constructor(title: string, description: string) {
