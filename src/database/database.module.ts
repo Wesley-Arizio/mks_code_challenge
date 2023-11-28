e568e52dc6b160b513b55a88a151b0a447e71a61
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-11-28 13:07:43 -0300

    fix deploy configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,7 +11,7 @@ import { Movie } from 'src/movies/entities/movie.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: 5432,
+      port: Number(process.env.POSTGRES_PORT) || 5432,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
